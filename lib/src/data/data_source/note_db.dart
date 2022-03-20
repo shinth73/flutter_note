459c52d6d661336546a04056ffb4a19aad429e6d
th shin <shinth73@gmail.com>
2022-03-20 13:27:00 +0900

    first commit

--- lib/src/data/data_source/note_db.dart
+++ lib/src/data/data_source/note_db.dart
@@ -2,10 +2,3 @@ class NoteDb{
 
 }
 
-echo "# flutter_note" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/shinth73/flutter_note.git
-git push -u origin main
